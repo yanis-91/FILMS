c99c81cd692aa23158d645952f13c187a79a1c7a
yanis-91 <20230719@etud.univ-evry.fr>
2024-11-19 22:07:20 +0100

    first commit

---
+++ actors.mjs
@@ -0,0 +1,48 @@
+export const films = [
+    {
+        title: "Inception",
+        year: 2010,
+        director: "Christopher Nolan",
+        actors: ["Leonardo DiCaprio", "Elliot Page", "Tom Hardy"]
+    },
+    {
+        title: "The Dark Knight",
+        year: 2008,
+        director: "Christopher Nolan",
+        actors: ["Christian Bale", "Heath Ledger", "Michael Caine"]
+    },
+    {
+        title: "Titanic",
+        year: 1997,
+        director: "James Cameron",
+        actors: ["Leonardo DiCaprio", "Kate Winslet", "Billy Zane", "Kathy Bates"]
+    },
+    {
+        title: "Avatar",
+        year: 2009,
+        director: "James Cameron",
+        actors: ["Sam Worthington", "Zoe Saldana", "Sigourney Weaver"]
+    },
+    {
+        title: "Mad Max: Fury Road",
+        year: 2015,
+        director: "George Miller",
+        actors: ["Tom Hardy", "Charlize Theron"]
+    }
+];
+
+export const actorSalaries = new Map([
+    ["Leonardo DiCaprio", 200000],
+    ["Elliot Page", 1000],
+    ["Tom Hardy", 15000],
+    ["Christian Bale", 10000],
+    ["Heath Ledger", 5000],
+    ["Michael Caine", 2000],
+    ["Kate Winslet", 8000],
+    ["Billy Zane", 1000],
+    ["Kathy Bates", 3000],
+    ["Sam Worthington", 5000],
+    ["Zoe Saldana", 6000],
+    ["Sigourney Weaver", 7000],
+    ["Charlize Theron", 10000]
+]);
